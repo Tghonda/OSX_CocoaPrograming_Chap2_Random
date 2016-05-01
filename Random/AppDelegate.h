fcfafcb8d168a860a40fcd125fe9d53549b0b6d0
Tghonda <tghonda@gmail.com>
2016-05-01 18:05:30 +0900

    Initial Commit

---
+++ Random/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Random
+//
+//  Created by 本田忠嗣 on 2016/05/01.
+//  Copyright © 2016年 TadatsuguHonda. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
